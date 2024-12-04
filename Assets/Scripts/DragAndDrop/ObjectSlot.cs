953fca81a7337127e36e0aa1d4f2954f4cb766d0
santiagoPostacchini <postacchini.santiago.et21.21@gmail.com>
2024-12-04 20:28:25 -0300

    push

--- Assets/Scripts/DragAndDrop/ObjectSlot.cs
+++ Assets/Scripts/DragAndDrop/ObjectSlot.cs
@@ -7,6 +7,8 @@ public class ObjectSlot : MonoBehaviour, IDropHandler
 {
     private Barra _bar;
 
+    public bool tacho = false;
+
     private void Awake()
     {
         _bar = GetComponent<Barra>();
@@ -17,35 +19,47 @@ public class ObjectSlot : MonoBehaviour, IDropHandler
         Debug.Log("Drop " + this.name);
         if (eventData.pointerDrag != null)
         {
-            if (eventData.pointerDrag.GetComponent<Client>() != null)
+            if (!tacho)
             {
-                var c = eventData.pointerDrag.GetComponent<Client>();
-                if (_bar.SpaceAvailable())
+                if (eventData.pointerDrag.GetComponent<Client>() != null)
                 {
-                    eventData.pointerDrag.GetComponent<DragDrop>().canDrop = true;
-                    c.clientSeat.SetFree();
-                    _bar.GetClientToPosition(c);
+                    var c = eventData.pointerDrag.GetComponent<Client>();
+                    if (_bar.SpaceAvailable())
+                    {
+                        eventData.pointerDrag.GetComponent<DragDrop>().canDrop = true;
+                        c.clientSeat.SetFree();
+                        _bar.GetClientToPosition(c);
+                    }
+                    else
+                    {
+                        eventData.pointerDrag.GetComponent<DragDrop>().canDrop = false;
+                    }
                 }
-                else
-                {
-                    eventData.pointerDrag.GetComponent<DragDrop>().canDrop = false;
-                }
-            }
 
-            if (eventData.pointerDrag.GetComponent<Plato>() != null)
-            {
-                var p = eventData.pointerDrag.GetComponent<Plato>();
-                Debug.Log("Yo ordene " + p.foodName);
-                if (_bar.WhoOrderedThis(p) != null)
+                if (eventData.pointerDrag.GetComponent<Plato>() != null)
                 {
-                    eventData.pointerDrag.GetComponent<DragDrop>().canDrop = true;
-                    GameManager.instance.DeliverOrder(_bar.WhoOrderedThis(p), p, _bar);
-                }
-                else
-                {
-                    eventData.pointerDrag.GetComponent<DragDrop>().canDrop = false;
+                    var p = eventData.pointerDrag.GetComponent<Plato>();
+                    Debug.Log("Yo ordene " + p.foodName);
+                    if (_bar.WhoOrderedThis(p) != null)
+                    {
+                        eventData.pointerDrag.GetComponent<DragDrop>().canDrop = true;
+                        GameManager.instance.DeliverOrder(_bar.WhoOrderedThis(p), p, _bar);
+                    }
+                    else
+                    {
+                        eventData.pointerDrag.GetComponent<DragDrop>().canDrop = false;
+                    }
                 }
             }
+            else
+            {
+                if (eventData.pointerDrag.GetComponent<Plato>() != null)
+                {
+                    var p = eventData.pointerDrag.GetComponent<Plato>();
+                    FoodFactory.Instance.ReturnFood(p);
+                }
+            }
+            
         }
     }
 }
